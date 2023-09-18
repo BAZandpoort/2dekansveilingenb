d2f5a8ca3ab2b7744711ad1e8e4176aede52618b
Aalleepphh <alperen.tok@bazandpoort.be>
2023-09-18 16:20:55 +0200

    Update language_select.php

--- src/public/language_select.php
+++ src/public/language_select.php
@@ -1,16 +1,34 @@
 <div>
-  
 
-  <?php 
-    if (isset($_POST["knop"])){
-        
+
+    <?php
+    if (isset($_POST["knop"])) {
+
     } else {
         echo '
             <form method="post" action="language_select.php">
-                <button class="btn btn-wide">Wide</button>
+                <div class="form-control">
+                    <label class="label cursor-pointer">
+                    <span class="label-text">English</span> 
+                    <input type="radio" name="radio-lang" class="radio checked:bg-blue-500" checked />
+                    </label>
+                </div>
+                <div class="form-control">
+                    <label class="label cursor-pointer">
+                    <span class="label-text">Nederlands</span> 
+                    <input type="radio" name="radio-lang" class="radio checked:bg-blue-500" checked />
+                    </label>
+                </div>
+                <div class="form-control">
+                    <label class="label cursor-pointer">
+                    <span class="label-text">Français</span> 
+                    <input type="radio" name="radio-lang" class="radio checked:bg-blue-500" checked />
+                    </label>
+                </div>
+                <input type="submit" name="knop" value="Change" class="btn btn-active btn-primary">
             </form>
         ';
     }
-  ?>
+    ?>
 
 </div>
