78bca088a087e657abf6538d89f56080c4839771
samirelma <samir.elmazzouji@bazandpoort.be>
2023-09-22 11:37:09 +0200

    Fix nav bar

--- src/components/nav.php
+++ src/components/nav.php
@@ -1,8 +1,4 @@
-<div class="navbar bg-base-100">
-  <div class="flex-1">
-    <a class="btn btn-ghost normal-case text-xl">daisyUI</a>
-  </div>
-  <div class="navbar bg-base-100 shadow-sm mb-24">
+<div class="navbar bg-base-100 shadow-sm mb-24">
   <div class="navbar-start">
     <div class="dropdown">
       <label tabindex="0" class="btn btn-ghost lg:hidden">
@@ -37,16 +33,14 @@
       <li><a>Profile</a></li>
     </ul>
   </div>
+  
+ 
+  <div class="navbar-end">
 
-  <div class="flex-none gap-2">
-    <div class="form-control">
-      <input type="text" placeholder="Search" class="input input-bordered w-24 md:w-auto" />
-    </div>  
-    <div class="navbar bg-base-100">
-  <div class="flex-none gap-2">
+  </div>
 
-  <div class="flex-none">
-    <div class="dropdown dropdown-end">
+
+  <div class="dropdown dropdown-end">
       <label tabindex="0" class="btn btn-ghost btn-circle">
         <div class="indicator">
           <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
@@ -63,31 +57,31 @@
         </div>
       </div>
     </div>
-    <div class="dropdown dropdown-end">
+
+    <div class="">
+    <?php echo isset($_SESSION['user'])
+      ? '<div class="dropdown dropdown-end">
       <label tabindex="0" class="btn btn-ghost btn-circle avatar">
         <div class="w-10 rounded-full">
-          <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+          <img src="https://avatars.githubusercontent.com/u/64209400?v=4" />
         </div>
       </label>
-      <ul tabindex="0" class="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
+      <ul tabindex="0" class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
         <li>
           <a class="justify-between">
             Profile
+            <span class="badge">New</span>
           </a>
         </li>
-        <li><a>Settings</a></li>  
-         <div class="navbar-end">
-          <a href="<?php echo isset($_SESSION['user'])
-           ? '/logout'
-           : '/login'; ?>" class="badge"><?php echo isset($_SESSION['user'])
-           ? 'Logout'
-           : 'Login'; ?></a>
-           </div>
-          </div>
+        <li><a>Settings</a></li>
+        <li><a href="/logout"> logout</a></li>
+        
       </ul>
-    </div>
+    </div>'
+      : '<a href="/login" class="btn"> Login</a>'; ?>
+      
   </div>
-</div>
-</div>
-</div>
+    
+    
+
 </div>
