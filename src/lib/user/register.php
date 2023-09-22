13a301d9048f6efc24acebbf64a3382bfaa136f2
Cédric <work@cedricverlinden.com>
2023-09-22 12:43:38 +0200

    Fix issue with relative paths when including other files

--- src/lib/user/register.php
+++ src/lib/user/register.php
@@ -5,7 +5,7 @@ if (!isset($_POST['register'])) {
   exit();
 }
 
-require_once '../../../config.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/config.php';
 require_once LIB . '/util/util.php';
 require_once LIB . '/authentication/authentication.php';
 
