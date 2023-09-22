b0566345b0f55f52f2bde27e7a4a7ff0e4851fa8
Aalleepphh <alperen.tok@bazandpoort.be>
2023-09-22 14:30:39 +0200

    Update language_select.php

--- src/public/language_select.php
+++ src/public/language_select.php
@@ -39,7 +39,7 @@
                     <input type="radio" name="radio-lang" value=2 class="radio checked:bg-blue-500" unchecked />
                     </label>
                 </div>
-                <input type="submit" name="knop" value="Change" class="btn btn-active btn-primary">
+                <input type="submit" name="knop" value="✔️" class="btn btn-active btn-neutral">
             </form>
         ';
     }
