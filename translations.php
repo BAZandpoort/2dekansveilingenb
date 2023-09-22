308528f3bc395878cc5ab5662c1bf711ac9450e0
Aalleepphh <alperen.tok@bazandpoort.be>
2023-09-22 12:01:48 +0200

    added new page translations.php

---
+++ translations.php
@@ -0,0 +1,14 @@
+<?php
+
+$translations = [
+  'services' => [
+    0 => 'Services',
+    1 => 'Services',
+    2 => 'Services',
+  ],
+  'txt_design' => [
+    'english' => 'Services',
+    'dutch' => 'Services',
+    'french' => 'Services',
+  ],
+];
