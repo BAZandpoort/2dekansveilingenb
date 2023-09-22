7056ae5875411a317ef451cf338f18825ce47715
Xander <xnauwelaerts@gmail.com>
2023-09-22 10:28:47 +0200

    Update footer.php

--- src/components/footer.php
+++ src/components/footer.php
@@ -20,6 +20,20 @@
       <a class="link link-hover">Privacybeleid</a>
       <a class="link link-hover">Cookiebeleid</a>
     </nav>
+
+    <div class="navbar-end">
+      <?php 
+       
+require_once '../../../config.php';
+require_once DATABASE . '/connect.php';
+      ?>
+    <a href="<?php echo )
+      ? '/logout'
+      : '/login'; ?>" class="btn">
+      <?php echo isset($_SESSION['user'])
+  ? 'Light mode'
+  : 'Dark mode'; ?></a>
+  </div>
   </footer>
   <footer class="footer px-10 py-4 border-t bg-base-200 text-base-content border-base-300">
     <aside class="items-center grid-flow-col">
@@ -33,5 +47,6 @@
         <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="fill-current"><path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path></svg></a>
       </div>
     </nav>
-  </footer>
+
+   
 </div>
