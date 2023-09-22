1016f272f89540a2ce72c9d2c5497d9c4c787608
Aalleepphh <alperen.tok@bazandpoort.be>
2023-09-22 14:07:02 +0200

    Added list of translations to translations page
    consisting of translations for the footer

--- translations.php
+++ translations.php
@@ -1,14 +1,71 @@
 <?php
 
-$translations = [
+$translate = [
   'services' => [
     0 => 'Services',
-    1 => 'Services',
-    2 => 'Services',
+    1 => 'Diensten',
+    2 => 'Service',
   ],
-  'txt_design' => [
-    'english' => 'Services',
-    'dutch' => 'Services',
-    'french' => 'Services',
+  'design' => [
+    0 => 'Design',
+    1 => 'Ontwerp',
+    2 => 'Désign',
   ],
+  'marketing' => [
+    0 => 'Marketing',
+    1 => 'Marketing',
+    2 => 'Mercatique',
+  ],
+  'advertisement' => [
+    0 => 'Advertisement',
+    1 => 'Advertentie',
+    2 => 'Publicité',
+  ],
+  'business' => [
+    0 => 'Business',
+    1 => 'Bedrijf',
+    2 => 'Enterprise',
+  ],
+  'about us' => [
+    0 => 'About us',
+    1 => 'Over ons',
+    2 => 'À propos de nous',
+  ],
+  'vacancies' => [
+    0 => 'Vacancies',
+    1 => 'Vacatures',
+    2 => 'Postes vacants',
+  ],
+  'press kit' => [
+    0 => 'Press kit',
+    1 => 'Perskit',
+    2 => 'Dossier de presse',
+  ],
+  'legal' => [
+    0 => 'Legal',
+    1 => 'Juridisch',
+    2 => 'Juridique',
+  ],
+  'terms of use' => [
+    0 => 'Terms of Use',
+    1 => 'Gebruiksvoorwaarden',
+    2 => "Conditions d'utilisation",
+  ],
+  'privacy policy' => [
+    0 => 'Privacy Policy',
+    1 => 'Privacybeleid',
+    2 => 'Politique de confidentialité',
+  ],
+  '2nd-chance auctions' => [
+    0 => '2nd-chance auctions',
+    1 => '2dekans veilingen',
+    2 => '2ème-chance enchères',
+  ],
+  'reliable tech' => [
+    0 => 'Reliable technology since 2023',
+    1 => 'Betrouwbare technologie sinds 2023',
+    2 => 'Technologie fiable depuis 2023',
+  ],
+  
+  
 ];
