472fbe6cd0fbca505de12bd6ed8cc91c25f2ee6b
Aalleepphh <alperen.tok@bazandpoort.be>
2023-09-22 10:42:17 +0200

    Update language_select.php

--- src/public/language_select.php
+++ src/public/language_select.php
@@ -8,11 +8,16 @@
         $_SESSION["lang"] = $_POST["radio-lang"];
 
         $success_msg = array("You have successfully switched the website's language to English.", "U hebt succesvol de taal van de website veranderd.", "Vous avez changé le langue de site web avec succès.");
-        echo $success_msg[$_SESSION["lang"]];
+        echo '
+            <div class="alert alert-success">
+                <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+                <span>'.$success_msg[$_SESSION["lang"]].'</span>
+            </div>
+        ';
 
     } else {
         echo '
-            <form method="post" action="language_select.php">
+            <form method="post" action="language-select">
                 <div class="form-control">
                     <label class="label cursor-pointer">
                     <span class="label-text">English</span> 
