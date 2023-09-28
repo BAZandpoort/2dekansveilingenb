152498a4126acf214603c5da3423da2766d3c7c7
Imad4747 <90533521+Imad4747@users.noreply.github.com>
2023-09-28 08:37:10 +0200

    add succes_mapping

--- src/public/login.php
+++ src/public/login.php
@@ -22,7 +22,7 @@ if ($error) {
     <div class="alert alert-success w-full max-w-xs mx-auto mb-8">
         <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"  /></svg>
         <span>' .
-    (ERROR_MAPPING[$error] ?? 'success') .
+    (SUCCES_MAPPING[$error] ?? 'success') .
     '</span>
     </div>';
 
