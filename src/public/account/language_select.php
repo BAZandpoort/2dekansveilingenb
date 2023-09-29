20464f09af06d9c5251873564030bd87588a03b4
Aalleepphh <alperen.tok@bazandpoort.be>
2023-09-29 16:10:15 +0200

    Update language_select.php

--- src/public/account/language_select.php
+++ src/public/account/language_select.php
@@ -5,14 +5,11 @@
     if (isset($_POST["knop"])) {
         // Each number corresponds to a language. 0 = English, 1 = Nederlands, 2 = Français.
 
-        $_SESSION["lang"] = $_POST["radio-lang"];
-        $lng = $_SESSION["lang"];
-
         $txt_success = $language["You have successfully switched the website's language to English."];
         echo '
             <div class="alert alert-success">
                 <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
-                <span>'.$txt_success[$lng].'</span>
+                <span>'.$txt_success.'</span>
             </div>
         ';
 
