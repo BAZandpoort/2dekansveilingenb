9305ad53b3bcf9e2dbcef6323d09fabbee2c4c62
samirelma <samir.elmazzouji@bazandpoort.be>
2023-09-29 16:21:22 +0200

    add bid button

--- routes.php
+++ routes.php
@@ -61,4 +61,11 @@ $routes = [
     'nav' => false,
     'footer' => false,
   ],
+  '/catalog/bied' => [
+    'view' => '/catalog/bied.php',
+    'title' => 'bieden',
+    'auth_roles' => ['member'],
+    'nav' => true,
+    'footer' => true,
+  ],
 ];
