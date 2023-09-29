01a3c70a6b94fef0190ccc1202f991698ccb7808
Xander <xnauwelaerts@gmail.com>
2023-09-29 12:47:49 +0200

    aanpassingen werkt nog niet

--- src/public/member/myProducts.php
+++ src/public/member/myProducts.php
@@ -3,6 +3,15 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/config.php';
 require_once LIB . '/util/util.php';
 include_once LIB . '/catalog/products.php';
 
-myProducts();
+  
+$userid = isset($_SESSION['user']) ? $_SESSION['user']['id'] : null;
+$query = "SELECT * FROM products WHERE userid = ?";
+$products = fetch($query, ['type' => 'i', 'value' => $userid]);
 
+if (empty($products)){
+    myProducts();
+}else{
+    echo"<h2>u hebt nog geen producten</h2>";
+    print"test";
+}
 ?>
