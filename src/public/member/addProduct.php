a8a679b86b9ac6bb05f025c5106084a4af513295
Aalleepphh <alperen.tok@bazandpoort.be>
2023-09-29 14:38:20 +0200

    Update addProduct.php

--- src/public/member/addProduct.php
+++ src/public/member/addProduct.php
@@ -87,7 +87,7 @@ if ($error) {
     </div>
 
     <div class="flex flex-col gap-2 w-full max-w-xs">
-      <label for="auction_duration"><b>For auctions only</b> | Auction ending date</label>
+      <label for="auction_duration">Auction end date | <b>For auctions only</b></label>
       <input
       name="auction_duration"
       id="auction_duration"
