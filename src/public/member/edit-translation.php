6f8829374b723943d3c2db4946195364fcbe603d
Aalleepphh <144694586+alperenZP@users.noreply.github.com>
2023-10-03 21:15:52 +0200

    Update edit-translation.php

--- src/public/member/edit-translation.php
+++ src/public/member/edit-translation.php
@@ -21,6 +21,13 @@ echo '<h1>Edit translations of ' . $_GET["location"] . ' </h1>';
 ?>
 
 <div>
+    <div class="dropdown">
+    <label tabindex="0" class="btn m-1">Select page to edit</label>
+        <ul tabindex="0" class="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
+            <li><a>Item 1</a></li>
+            <li><a>Item 2</a></li>
+        </ul>
+    </div>
     <div class="overflow-x-auto">
     <table class="table table-xs">
         <thead>
