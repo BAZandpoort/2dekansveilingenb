07b29eb2f98c5653325a39c89f8679ab094dc947
Xander <xnauwelaerts@gmail.com>
2023-10-03 11:02:58 +0200

    centered stuff

--- src/public/product.php
+++ src/public/product.php
@@ -19,39 +19,42 @@
 
 ?>
 <div class = "flex">
-    <div class="flex-[1] rounded-3xl h-1/2">
-        <img class="w-full h-full rounded-3xl" src="<?php echo $products["imageUrl"];?>"  />
-        <div>
-        <h2 class="card-title mt-8 mx-8"><?php echo $products["name"] ?></h2>
-          <div class="mx-8">  <?php echo $products["description"] ?></div>
-        </div>
-        <div>
-        <h2 class="card-title mt-8 mx-8"> about seller:</h2>
-           <div>
-                <div class="mt-4 mx-8"><?php echo $seller["firstname"]," ", $seller["lastname"] ?></div>
-                <div class="mt-4 mx-8"><?php echo $seller["about"]?></div>
-            </div>
-        </div>
+    <div class="flex-[1.1] rounded-3xl h-1/2">
+        <img class="w-full h-full rounded-3xl" src="<?php echo $products["imageUrl"];?>"  /> 
     </div>
-    <div class="flex-[1] flex items-stretch">
-        <div class="card w-full bg-base-96 shadow-xl">
-            <div class="radial-progress mx-auto mt-8" style="--value:70;">
-                time
-            </div>
-            <div class="card-body items-center text-center">
-                <table class="table-auto"> 
-                    <tr>
+    <div class="flex-[0.9] flex items-stretch card w-full shadow-xl ">
+        <div class="card w-full my-auto">
+                <div class="radial-progress mx-auto" style="--value:70;">
+                    time
+                </div>
+                <div class="card-body items-center text-center">
+                    <table class="table-auto"> 
+                        <tr>
                             <td class="text-xl ">bidprice</td>
                             <td class = "text-xl" >buyprice</td>
-                    </tr>
-                    <tr>
+                        </tr>
+                        <tr>
                             <td>(current bid price)</td>
                             <td><?php echo $products["price"]?></td>
-                    </tr>
-                </table>
-                <a href="Bidden.php" class="btn btn-primary">bit Now</a>
-            </div>
+                        </tr>
+                    </table>
+                    <a href="Bidden.php" class="btn btn-primary">bit Now</a>
+                </div>
         </div>
     </div>
 </div>
+<div class = "flex">
+    <div>
+    <h2 class="card-title mt-8 mx-8"><?php echo $products["name"] ?></h2>
+    <div class="mx-8">  <?php echo $products["description"] ?></div>
+    </div>
+    <div>
+        <h2 class="card-title mt-8 mx-8"> about seller:</h2>
+    <div>
+        <div class="mt-4 mx-8"><?php echo $seller["firstname"]," ", $seller["lastname"] ?></div>
+        <div class="mt-4 mx-8"><?php echo $seller["about"]?></div>
+    </div>
+    </div>
+</div>
+
 
