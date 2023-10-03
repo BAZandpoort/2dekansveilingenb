63c1765bf4dea8ceb4b7928aec99ff99943e11d6
Aalleepphh <alperen.tok@bazandpoort.be>
2023-10-03 11:17:45 +0200

    Update nav.php

--- src/components/nav.php
+++ src/components/nav.php
@@ -11,6 +11,10 @@ if ($userid) {
   $theme = $data['theme'] === 'dark' ? 'light' : 'dark';
   $language = $data['language'];
 }
+
+if (!isset($language)){
+  $language = "english";
+}
 ?>
 
 <div class="navbar bg-base-100">
@@ -63,7 +67,7 @@ if ($userid) {
             </li>
             <li><a href="src/lib/account/change-theme.php" >Switch to ' . $theme . '</a></li>
             <li><a>Settings</a></li>
-            <li><a href="/account/logout"> logout '.$language.'</a></li>
+            <li><a href="/account/logout"> logout </a></li>
             
           </ul>
         </div>
