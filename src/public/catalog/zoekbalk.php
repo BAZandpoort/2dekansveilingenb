e7b44b1125f8f9c35dfb7914433c7a31a7dfa7ba
AbdullahafloefZP <129497382+AbdullahafloefZP@users.noreply.github.com>
2023-10-06 22:27:33 +0200

    hover share and favorite (#55)
    * hover share and favorite
    
    * copy product url
    
    * update link copy and header
    
    * Fix layout issue

--- src/public/catalog/zoekbalk.php
+++ src/public/catalog/zoekbalk.php
@@ -29,26 +29,37 @@ if (isset($_POST['submit'])) {
     if ($result->num_rows > 0) {
         while ($row = mysqli_fetch_array($result)) {
           echo '
-          <a class="card card-compact w-60 inline-block ml-2 my-6 bg-white hover:bg-gray-100 relative">
-              <form method="post" action="/catalog/product?id=' .$row["id"]. '">
-                  <img src="' .$row["imageUrl"]. '" class=" max-h-44 w-64 m-auto object-cover">
-                  <div class="card-body">
-                  <h2 class="card-title text-base">
-                    ';
-                    if(strlen($row["name"]) > 20) {
-                    echo substr_replace($row["name"], "...", 21);} else { echo $row["name"];}
-                    echo '</h2>
-                    <div class="text-base mt-5"><p>€' .$row["price"]. '</p></div>
-                      <input type="hidden"name="image"value="' .$row["imageUrl"]. '">
-                      <input type="hidden"name="name"value="' .$row["name"]. '">
-                      <input type="hidden"name="price"value="' .$row["price"]. '">
-                      <button type="submit" class="btn btn-warning mr-0 mx-32 -mt-11" name="bied" >Bid</button>
-                      <p class="text-base text-center"><span id="timer">00:00:00</span></p>
-                      <p class="text-base text-center -mt-3">Status: <span id="status">Open</span></p>
-                  </div>
-              </form>
-          </a>
-          ' ;
+                <div class="card card-compact w-60 inline-block ml-2 my-6 bg-white relative">
+                    <form method="post" action="index.php?id=' . $product["id"] . '">
+                    <div class="hover-card">
+                    <div class="dropdown dropdown-end">
+                        <img src="' . $product["imageUrl"] . '" class="max-h-44 w-64 m-auto object-cover">
+                            <label tabindex="0">
+                                <div class="hover-dots cursor-pointer absolute -mt-56 right-2 text-7xl text-amber-400 opacity-0 block">...</div>
+                            </label>
+                              <ul tabindex="0" class="dropdown-content z-[1] p-2 shadow bg-base-100 rounded-box w-26 -mt-36 mr-2">
+                                <a href="#"><svg href="#" class="w-6 h-6 hover:text-black text-gray-600 inline-block mx-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 21 19">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 4C5.5-1.5-1.5 5.5 4 11l7 7 7-7c5.458-5.458-1.542-12.458-7-7Z"/>
+                                </svg></a>
+                                <a href="#"><svg class="w-5 h-5 hover:text-black text-gray-600 inline-block mx-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m5.953 7.467 6.094-2.612m.096 8.114L5.857 9.676m.305-1.192a2.581 2.581 0 1 1-5.162 0 2.581 2.581 0 0 1 5.162 0ZM17 3.84a2.581 2.581 0 1 1-5.162 0 2.581 2.581 0 0 1 5.162 0Zm0 10.322a2.581 2.581 0 1 1-5.162 0 2.581 2.581 0 0 1 5.162 0Z"/>
+                                </svg></a>
+                              </ul>
+                        </div>   
+                    </div> 
+                        <div class="card-body">
+                            <h2 class="card-title text-base">' . $productName . '</h2>
+                            <div class="text-base mt-5"><p>€' . $product["price"] . '</p></div>
+                            <input type="hidden" name="image" value="' . $product["imageUrl"] . '">
+                            <input type="hidden" name="name" value="' . $product["name"] . '">
+                            <input type="hidden" name="price" value="' . $product["price"] . '">
+                            <button type="submit" class="btn btn-warning mr-0 mx-32 -mt-11" name="bied">Bid</button>
+                            <p class="text-base text-center"><span id="timer">00:00:00</span></p>
+                            <p class="text-base text-center -mt-3">Status: <span id="status">Open</span></p>
+                        </div>
+                    </form>
+                </div>
+            ';
         }
     } else {
         echo "Geen overeenkomende producten gevonden.";
@@ -57,3 +68,8 @@ if (isset($_POST['submit'])) {
     
 }
 ?>
+<style>
+.hover-card:hover .hover-dots {
+    opacity: 1;
+}
+</style>
