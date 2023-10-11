227af02d3a0c3570c3b80e6ab3d856fc4e40ca91
samirelma <samir.elmazzouji@bazandpoort.be>
2023-10-11 12:25:57 +0200

    fix product-verwijderen

--- src/lib/member/product-verwijderen.php
+++ src/lib/member/product-verwijderen.php
@@ -1,33 +1,36 @@
 <?php
 
+ // Verbinding maken met de database
+ require_once $_SERVER['DOCUMENT_ROOT'] . '/config.php';
+ require_once LIB . '/util/util.php';
+ require_once DATABASE . '/connect.php';
 
 // Controleer of een id is meegegeven via de URL
 if (isset($_GET['productid'])) {
     $id = $_GET['productid'];
 
-    // Verbinding maken met de database
-    require_once $_SERVER['DOCUMENT_ROOT'] . '/config.php';
-    require_once LIB . '/util/util.php';
-    require_once DATABASE . '/connect.php';
-
-    
-
+   
 
 
     // SQL-query om het product te verwijderen
     $query = "DELETE FROM products WHERE id = ?";
+
     $deleteData = insert($query, ['type' => 'i', 'value' => $id]);
 
     if ($deleteData) {
-        echo "Product succesvol verwijderd.";
-    } else {
-        echo "Fout bij het verwijderen van het product.";
-    }
+        header("Location: /dashboard/product-verwijderen?error=success");
 
+    } else {
+        header("Location: /dashboard/product-verwijderen?error=delete-product-failed");
+
+    }
+    var_dump($deleteData);  
+
+
+   
 } else {
     echo "Geen id meegegeven.";
 }
 
- 
-return;
+
 ?>
