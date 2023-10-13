0133a66e107f49266cac54f5e29fe7f9768c3b0a
Imad4747 <90533521+Imad4747@users.noreply.github.com>
2023-10-13 13:53:28 +0200

    dahsboard

--- src/public/dashboard/dashboard.php
+++ src/public/dashboard/dashboard.php
@@ -29,29 +29,22 @@
 }
 
 .row1 .rectangle-div{
-  width: 200px;
-  height: 100px;
+  width: 250px;
+  height: 150px;
   margin-top: 40px;
   margin-right: 10px;
   margin: 20px;
   border-radius: 25px;
   background-color: black;
+ 
   transition: transform 0.2s, box-shadow 0.2s;
 }
+
 .rectangle-div:hover{
-  width: 300px;
-  height: 150px;
-  transform: scale(1.1);
   box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.5);
 
 }
-.rectangle-div p{
-  font-size: 18px;
-  display: flex;
-  align-content: center;
-  justify-content: center;
 
-}
 .hover-text{
    position: absolute;
   top: 0;
@@ -74,7 +67,7 @@
   height: 100px;
   margin-right: 10px;
   border-radius: 25px;
-  margin: 20px;
+  margin: 25px;
   background-color: black;
 justify-content: center;
 align-content: center;
@@ -87,9 +80,6 @@ svg {
   margin-top: 10px;
 }
 .square-div:hover{
-  width: 110px;
-  height: 110px;
-  transform: scale(1.1);
   box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.5);
 
 }
@@ -109,21 +99,26 @@ svg {
     <div class="row row1">
       <a href="/catalog/products">
       <div class="rectangle-div">
-        <p>Producten</p>
-        <?php
-require_once LIB . '/util/util.php';
-require_once DATABASE . '/connect.php';
-$sql = "SELECT COUNT(name) as aantal FROM products";
-$aantalp = fetch($sql);
+        <?php 
+         require_once DATABASE . '/connect.php';
 
-   ?>
-        <a class="hover-text">Het aantal producten zijn <?php echo $aantalp['aantal']; ?></a>
+        $sql = "SELECT COUNT(name) as aantal FROM products";
+        $aantalP = fetch($sql);
+      
+        ?>
+        <p class="font-bold mt-5 ml-3 justify-center flex">Jouw producten: </p>
+        <p class="font-bold mt-0 ml-3 justify-center flex text-2xl mt-6"><?php echo $aantalP['aantal']; ?></p>
+        <a class="hover-text"></a>
       </div>
       </a>
-      <div class="rectangle-div"><p>Wisnt</p></div>
       <div class="rectangle-div">
-        <p>Producten Toevoegen</p>
-        <a class="hover-text">Wil je een product toevoegen?</a>
+      <p class="font-bold mt-5 ml-3 justify-center flex">Jouw verkochte producten: </p>
+        <p class="font-bold mt-0 ml-3 justify-center flex text-2xl mt-6">0</p>
+      </div>
+      <div class="rectangle-div">
+      <p class="font-bold mt-5 ml-3 justify-center flex">Jouw winst: </p>
+        <p class="font-bold mt-0 ml-3 justify-center flex text-2xl mt-6">0</p>
+        <a class="hover-text"></a>
       </div>
     </div>
     <div class="row row2">
@@ -134,11 +129,11 @@ $aantalp = fetch($sql);
   <input type="checkbox" />
   
   
-  <svg class="swap-on fill-current w-20 h-20" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z" fill="grey"/>
+  <svg class="swap-on fill-current w-20 h-20" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 28"><path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z" fill="grey"/>
 </svg>
   
   
-  <svg class="swap-off fill-current w-20 h-20" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z" fill="grey"/></svg>
+  <svg class="swap-off fill-current w-20 h-20" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 25 28"><path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z" fill="grey"/></svg>
   
 </label>
       </div>
