edbbf41428bbb1462cfe07a3aa13bdad0a591024
Aalleepphh <alperen.tok@bazandpoort.be>
2023-10-16 15:17:29 +0200

    added ability to submit reports to a table

--- src/public/catalog/report-abuse.php
+++ src/public/catalog/report-abuse.php
@@ -1,4 +1,10 @@
 <?php
+if (!isset($_GET['productid'])) {
+    header('location: /');
+    exit();
+  }
+  
+
 if (!isset($_SESSION['user'])) {
   header('Location: /');
   exit();
@@ -7,9 +13,13 @@ if (!isset($_SESSION['user'])) {
 require_once $_SERVER['DOCUMENT_ROOT'] . '/config.php';
 require_once LIB . '/util/util.php';
 
+$productId = $_GET['productid'];
+$query = 'SELECT * FROM products WHERE id = ?';
+$productData = fetch($query, ['type' => 'i', 'value' => $productId]);
 ?>
 
 <h1 class="text-center text-4xl font-bold mb-12">Report abuse</h1>
+<h2 class="text-center text-2xl font-bold mb-12">Product: <u><?php echo $productData["name"]?></u></h2>
 
 <form action="/src/lib/user/member/report-abuse.php" method="post" class="flex flex-col items-center justify-center gap-4 max-w-2xl mx-auto">
   <input type="hidden" name="productid" id="productid" value="<?php echo $_GET["productid"]?>">
@@ -19,7 +29,7 @@ require_once LIB . '/util/util.php';
       <label class="label">
         <span class="label-text">Type of abuse</span>
       </label>
-      <select name="category" class="select select-bordered w-full">
+      <select name="typeOfAbuse" class="select select-bordered w-full">
         <option value="Deception">Deceptive or misleading content</option>
         <option value="Trafficking">Trafficking of illegal products</option>
         <option value="Hate">Hateful or inflammatory conduct</option>
