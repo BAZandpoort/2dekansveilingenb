d04a024079d18ca02c65bd07a8626c0fadbf42db
AbdullahafloefZP <129497382+AbdullahafloefZP@users.noreply.github.com>
2023-10-16 15:10:56 +0200

    start chatfunctie

---
+++ src/public/chats/chat.php
@@ -0,0 +1,11 @@
+<div class="wrapper">
+    <section class="chat-area">
+        <header>
+            <img src="#">
+            <div class="details">
+                <span>Abdullah</span>
+                <p>Active now</p>
+            </div>
+        </header>
+    </section>
+</div>
---
+++ src/public/chats/users.php
@@ -0,0 +1,18 @@
+<div class="wrapper">
+    <section class="users">
+        <header>
+            <div class="content">
+                <img src="#">
+                <div class="details">
+                    <span>Abdullah</span>
+                    <p>Active now</p>
+                </div>
+            </div>
+        </header>
+        <div class="search">
+            <span class="text">Select a user to start chatting</span>
+            <input type="text" placeholder="Enter name">
+            <input type="submit" value="search">
+        </div>
+    </section>
+</div>
