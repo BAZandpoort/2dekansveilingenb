0f0b1f77844c09a1a459d9fc229976448748fd3c
Cédric <work@cedricverlinden.com>
2023-10-18 16:46:52 +0200

    Fix that products can be found by category name

--- src/public/catalog/products.php
+++ src/public/catalog/products.php
@@ -4,84 +4,53 @@ require_once LIB . '/catalog/products.php';
 require_once COMPONENTS . '/product-card.php';
 
 $products = [];
-$categoryid = $_GET['category'] ?? null;
+$categoryname = $_GET['category'] ?? null;
 
-
+$minPrice = $_GET['minPrice'] ?? 0;
+$maxPrice = $_GET['maxPrice'] ?? 0;
 
 if (isset($_GET['search'])) {
   $searchTerm = $_GET['search'];
   $query = "SELECT * FROM products WHERE MATCH(name, description) AGAINST(? IN BOOLEAN MODE)";
-  $params = ['type' => 's', 'value' => "$searchTerm*"];
-  
-  if (isset($_GET['start_price']) && isset($_GET['end_price'])) {
-    $start_price = $_GET['start_price'];
-    $end_price = $_GET['end_price'];
-    $query .= " AND price BETWEEN ? AND ?";
-    $products = fetchSingle($query, $params, ['type' => 'i', 'value' => $start_price], ['type' => 'i', 'value' => $end_price] );
-  } else {
-    $products = fetchSingle($query, $params);
-  }
+  $products = fetchSingle($query, ['type' => 's', 'value' => "$searchTerm*"]);
+
+} else if (isset($_GET['minPrice']) && isset($_GET['maxPrice'])) {
+  $query = "SELECT * FROM products WHERE price BETWEEN ? AND ?";
+  $products = fetchSingle($query, ['type' => 'i', 'value' => $minPrice], ['type' => 'i', 'value' => $maxPrice]);
+} else if ($categoryname) {
+  $categoryId = fetchSingle('SELECT * FROM product_categories WHERE name = ?', ['type' => 's', 'value' => $categoryname])[0]['id'];
+  $products = fetchSingle('SELECT * FROM products WHERE categoryid = ?', ['type' => 'i', 'value' => $categoryId]);
 } else {
-  if (isset($_GET['start_price']) && isset($_GET['end_price'])) {
-    $start_price = $_GET['start_price'];
-    $end_price = $_GET['end_price'];
-    $query = "SELECT * FROM products WHERE price BETWEEN ? AND ?";
-    $products = fetchSingle($query, ['type' => 'i', 'value' => $start_price], ['type' => 'i', 'value' => $end_price]);
-  } else {
-    $products = getAllProducts();
-  }
+  $products = getAllProducts();
 }
-
-if ($categoryid) {
-  $products = fetchSingle('SELECT * FROM products WHERE categoryid = ?', ['type' => 'i', 'value' => $categoryid]);
-}
-
-
 
 echo '
 <div class="w-full flex flex-col md:flex-row gap-4 p-8 md:pr-40">
-  <div class="hidden md:block md:flex-[.4] bg-base-300 rounded-2xl">
-  <div class="card-body"> 
-  <form action="" method="get">
-    <div class="row"> 
-      <div class="col-md-4"> 
-        <label for="">Start Price</label> 
-        <input type="text" name="start_price" value="" placeholder="Type here" class="input input-bordered input-success w-full max-w-xs" ';
-        if(isset($_GET['start_price'])) echo $_GET['start_price'];  else{echo'100';};
-         echo'" class="form-control">  
-      </div> 
-      <div class="col-md-4"> 
-        <label for="">End Price</label> 
-        <input type="text" name="end_price" value="" placeholder="Type here" class="input input-bordered input-success w-full max-w-xs" ';
-        if(isset($_GET['end_price'])) echo $_GET['end_price']; else{echo'900';}; 
-         echo'"class="form-control"> 
-      </div> 
-      <br>
-      <div class="col-md-4"> 
-        <button class="btn btn-outline btn-success">Klik hier</button>
-      </div> 
+  <div class="hidden md:block md:flex-[.4] h-fit bg-base-300 py-8 rounded-2xl">
+    <div>
+      <form method="get" class="flex flex-col px-4 gap-8">
+        <div class="flex flex-col gap-4">
+          <div class="form-control w-full max-w-xs">
+            <label class="label">
+              <span id="priceMinLabel" class="label-text">€' . $minPrice . '</span>
+            </label>
+            <input id="priceMin" name="minPrice" type="range" min="0" max="100" value="' . $minPrice . '" class="range range-accent" />
+          </div>
+          <div class="form-control w-full max-w-xs">
+            <label class="label">
+              <span id="priceMaxLabel" class="label-text">€' . $maxPrice . '</span>
+            </label>
+            <input id="priceMax" name="maxPrice" type="range" min="0" max="100" value="' . $maxPrice . '" class="range range-accent" />
+          </div>
+        </div>
+
+        <button class="btn btn-primary">Filter</button>
+      </form>
     </div>
-    <input type="hidden" name="search" value="';
-    if(isset($_GET['search'])) echo $_GET['search'];
-    echo '">
-  </form>
-  </div>
   </div>
   ';
 
-
-
-// if(isset($_GET['start_price']) && isset($_GET['end_price']) && !empty($_GET['start_price']) && !empty($_GET['end_price'])){
-//   $start_price = $_GET['start_price'];
-//   $end_price = $_GET['end_price'];
-//   $query = "SELECT * FROM products WHERE price BETWEEN $start_price AND $end_price";
-//   $products = fetch($query);
-  
-// }
   echo '
-
-
-  
   <div class="hidden md:flex divider divider-horizontal"></div> 
 
   <div class="flex flex-row flex-wrap gap-8 flex-[1.6]">
@@ -113,4 +82,18 @@ echo '
 ';
 ?>
 
-  
+<script defer>
+  const rangeOne = document.getElementById('priceMin');
+  const rangeTwo = document.getElementById('priceMax');
+
+  const rangeOneLabel = document.getElementById('priceMinLabel');
+  const rangeTwoLabel = document.getElementById('priceMaxLabel');
+
+  rangeOne.addEventListener('input', (e) => {
+    rangeOneLabel.textContent = `€${e.target.value}`;
+  });
+
+  rangeTwo.addEventListener('input', (e) => {
+    rangeTwoLabel.textContent = `€${e.target.value}`;
+  });
+</script>
