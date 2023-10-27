ce1622dee9d59caec9e717c70edb5c53630913cd
Imad4747 <90533521+Imad4747@users.noreply.github.com>
2023-10-27 14:23:41 +0200

    dashboard-koper af

--- src/public/dashboard-koper/dashboard.php
+++ src/public/dashboard-koper/dashboard.php
@@ -35,9 +35,20 @@ $data = fetch(
 echo $data['aantal'];
 }
 
+function purchasedCount(){
+  
 
+$userid = $_SESSION['user']['id'];
 
-
+$data = fetch(
+  'SELECT COUNT(id) as aantal FROM user_purchases WHERE id =?',
+  [
+    'type' => 'i',
+    'value' => $userid,
+  ],
+);
+echo $data['aantal'];
+}
 
 
 
@@ -55,7 +66,7 @@ echo $data['aantal'];
     </div>
     <div class="text-center">
     <h1 class="text-center font-bold" style="margin: 10px 0;"><?php name(); ?></h1>
-    <button class="btn btn-outline " style="margin: 10px 0;">My Settings</button>
+    <a href="/account/settings/edit"><button class="btn btn-outline " style="margin: 10px 0;" >My Settings</button></a>
 </div>
   </div>
 </div>
@@ -64,25 +75,25 @@ echo $data['aantal'];
 
 
 <!----------------------------------------------------------------------------------------------------------------- -->
-<div class="inline-block align-top">
+<div class="inline-block align-top m-2">
 <div class="stats shadow">
   
 <div class="stat w-60 h-60 ">
     <div class="stat-title text-center text-xl font-bold">Favorite Products</div>
     <div class="stat-value text-center"><?php favorietenCount();?></div>
-    <div class="stat-desc">21% more than last month</div>
+    <div class="stat-desc">Your count of favorite products</div>
   </div>
   
   <div class="stat w-60 h-60">
     <div class="stat-title text-center font-bold text-xl">Ordered Products</div>
     <div class="stat-value text-center">0</div>
-    <div class="stat-desc">21% more than last month</div>
+    <div class="stat-desc">Your count of ordered products</div>
   </div>
   
   <div class="stat w-60 h-60">
     <div class="stat-title text-center font-bold text-xl">Purchased Products</div>
-    <div class="stat-value text-center">0</div>
-    <div class="stat-desc">21% more than last month</div>
+    <div class="stat-value text-center"><?php purchasedCount(); ?></div>
+    <div class="stat-desc">Your count of purchased products</div>
   </div>
   
 </div> 
@@ -94,55 +105,64 @@ echo $data['aantal'];
   <div class="stat ">
     <div class="flex items-center justify-center h-full">
       <div class="overflow-x-auto">
-        <table class="table w-90">
+        <table class="table w-auto">
           <!-- head -->
           <thead>
             <tr>
               <th class="text-sm">Purchase History</th>
               <th></th>
-              <th><a class="link text-sm">See all</a></th>
+              <th><a class="link text-sm" href="/dashboard/products/history">See all</a></th>
 
             </tr>
           </thead>
           <tbody>
-            <!-- row 1 -->
-            <tr class="mb-4">
-              <td>
-                <div class="flex items-center space-x-3">
-                  <div class="avatar">
-                    <div class="mask mask-squircle w-12 h-12">
-                      <img src="https://avatars.githubusercontent.com/u/64209400?v=4" alt="Avatar Tailwind CSS Component" />
-                    </div>
-                  </div>
-                  <div>
-                    <div class="font-bold">Hart Hagerty</div>
-                  </div>
-                </div>
-              </td>
-              <td>
-                ddddd
-              </td>
-              <td>$85.20</td>
-            </tr>
-            <!-- row 2 -->
-            <tr class="mb-4">
-              <td>
-                <div class="flex items-center space-x-3">
-                  <div class="avatar">
-                    <div class="mask mask-squircle w-12 h-12">
-                      <img src="https://avatars.githubusercontent.com/u/64209400?v=4" alt="Avatar Tailwind CSS Component" />
-                    </div>
-                  </div>
-                  <div>
-                    <div class="font-bold">Hart Hagerty</div>
-                  </div>
-                </div>
-              </td>
-              <td>
-                Zemlak, Daniel and Leannon
-              </td>
-              <td>$85.20</td>
-            </tr>
+          <?php
+            
+            $userid = $_SESSION['user']['id'];
+
+$query = "SELECT * FROM user_purchases WHERE id=?";
+$purchase = fetchSingle($query, ['type' => 'i', 'value' => $userid]);
+
+if(!$purchase){
+  echo '
+    <tr class="mb-4">
+    <td>
+      <div class="flex items-center space-x-3">
+        
+        <div>
+          <div class="font-bold">Geen producten</div>
+        </div>
+      </div>
+    </td>
+    <td>
+      
+    </td>
+    <td></td>
+  </tr>';
+}else{
+  foreach ($purchase as $row) {
+    echo '
+    <tr class="mb-4">
+    <td>
+      <div class="flex items-center space-x-3">
+        <div class="avatar">
+          <div class="mask mask-squircle w-12 h-12">
+            <img src="/public/images/'.$row['productImage'].'" alt="Avatar Tailwind CSS Component" />
+          </div>
+        </div>
+        <div>
+          <div class="font-bold">'.$row['productName'].'</div>
+        </div>
+      </div>
+    </td>
+    
+    <td>€'.$row['price'].'</td>
+  </tr>';
+  
+  }
+}
+?>
+          
           </tbody>
         </table>
       </div>
@@ -162,13 +182,13 @@ echo $data['aantal'];
   <div class="stat ">
     <div class="flex items-center justify-center ">
       <div class="overflow-x-auto">
-        <table class="table w-80 h-90">
+        <table class="table w-80">
           <!-- head -->
           <thead>
             <tr>
               <th class="text-sm">Favorite Products</th>
               <th></th>
-              <th><a class="link text-sm">See all</a></th>
+              <th><a class="link text-sm" href="/account/favorite">See all</a></th>
 
             </tr>
           </thead>
@@ -180,29 +200,52 @@ echo $data['aantal'];
 $query = "SELECT * FROM products JOIN favorieten ON (products.id = favorieten.id) WHERE favorieten.userid = ?";
 $favorites = fetchSingle($query, ['type' => 'i', 'value' => $userid]);
 
-
-foreach ($favorites as $favorite) {
-	echo '
-  <tr class="mb-4">
-  <td>
-    <div class="flex items-center space-x-3">
-      <div class="avatar">
-        <div class="mask mask-squircle w-12 h-12">
-          <img src="'.$favorite['imageUrl'].'" alt="Avatar Tailwind CSS Component" />
+if(!$favorites){
+  echo '
+    <tr class="mb-4">
+    <td>
+      <div class="flex items-center space-x-3">
+        
+        <div>
+          <div class="font-bold">Geen producten</div>
         </div>
       </div>
-      <div>
-        <div class="font-bold">'.$favorite['name'].'</div>
+    </td>
+    <td>
+      
+    </td>
+    <td></td>
+  </tr>';
+}else{
+  foreach ($favorites as $favorite) {
+    if (strlen($favorite['description']) > 20) {
+      $favorite['description'] = substr_replace($favorite['description'], '...', 21);
+      
+    }
+    echo '
+    <tr class="mb-4">
+    <td>
+      <div class="flex items-center space-x-3">
+        <div class="avatar">
+          <div class="mask mask-squircle w-12 h-12">
+            <img src="/public/images/'.$favorite['imageUrl'].'" alt="Avatar Tailwind CSS Component" />
+          </div>
+        </div>
+        <div>
+          <div class="font-bold">'.$favorite['name'].'</div>
+        </div>
       </div>
-    </div>
-  </td>
-  <td>
-    '.$favorite['description'].'
-  </td>
-  <td>'.$favorite['price'].'</td>
-</tr>';
+    </td>
+    <td>
+      '.$favorite['description'].'
+    </td>
+    <td>'.$favorite['price'].'</td>
+  </tr>';
+  
+  }
 }
-            
+
+          
             
             ?>
           
@@ -226,10 +269,11 @@ foreach ($favorites as $favorite) {
     <thead>
       <tr>
         
-        <th>Name</th>
-        <th>Description</th>
-        <th>Price</th>
-        <th>Delivery</th>
+         <th class="text-sm">Ordered Products</th>
+          <th></th>
+          <th></th>
+          
+          <th><a class="link text-sm">See all</a></th>
       </tr>
     </thead>
     <tbody>
