5526ee0f1152b79d7134f26644ac350e55060ad8
samirelma <samir.elmazzouji@bazandpoort.be>
2023-11-14 10:29:41 +0100

    changed component

--- src/lib/user/member/factuur.php
+++ src/lib/user/member/factuur.php
@@ -1,7 +1,7 @@
 <?php
 require_once $_SERVER['DOCUMENT_ROOT'] . '/config.php';
 require_once LIB . '/util/util.php';
-require_once COMPONENTS . '/fpdf186.php';
+require_once COMPONENTS . '/fpdf186/fpdf.php';
 //require fpdf
 
 session_start();
