73a6ce4e6fb0669786f8077375cd2e666d0206ab
samirelma <samir.elmazzouji@bazandpoort.be>
2023-11-14 12:01:46 +0100

    works still needs to look good

--- src/lib/user/member/factuur.php
+++ src/lib/user/member/factuur.php
@@ -2,8 +2,37 @@
 require_once $_SERVER['DOCUMENT_ROOT'] . '/config.php';
 require_once LIB . '/util/util.php';
 require_once COMPONENTS . '/fpdf186/fpdf.php';
-//require fpdf
+require_once DATABASE . '/connect.php';
 
+
+// Start the session
 session_start();
+
+// Create a new PDF
+$pdf = new FPDF();
+$pdf->AddPage();
+$pdf->SetFont('Arial', 'B', 16);
+
+
+
+$userId = $_SESSION['user']['id'];
+
+$purchaseHistory = fetchSingle('SELECT * FROM user_purchases WHERE id = ?', ["type" => "i", "value" => $userId]);
+
+if ($purchaseHistory) {
+    foreach ($purchaseHistory as $purchase) {
+        $pdf->Cell(40, 10, 'Item Name: ' . $purchase['productName']);
+        $pdf->Ln();
+        $pdf->Cell(40, 10, 'Price: ' . $purchase['price']);
+        $pdf->Ln();
+        $pdf->Cell(40, 10, 'Date Purchased: ' . $purchase['timeOfPurchase']);
+        $pdf->Ln(); 
+        $pdf->Ln();  
+        
+    }
+} else {
+    $pdf->Cell(40, 10, 'No purchases found');
+}
+
+$pdf->Output('D', 'Invoice.pdf');
 ?>
-<h1>Factuur</h1>
