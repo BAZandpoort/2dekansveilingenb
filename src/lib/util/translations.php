e13a8b1e6bde1add9421900f3b3dcd37a4230fc7
Cédric <work@cedricverlinden.com>
2023-12-05 00:05:41 +0100

    Fix translation language fallback

--- src/lib/util/translations.php
+++ src/lib/util/translations.php
@@ -1,6 +1,6 @@
 <?php
 function translate($user, $str) {
-  $language = $user['language'];
+  $language = isset($user['language']) ? $user['language'] : $_SESSION['guest']['language'];
   $data = fetch('SELECT * FROM translations WHERE text_en = ?', ['type' => 's', 'value' => $str]);
 
   return (empty($data) ? $str : $data[$language]);
