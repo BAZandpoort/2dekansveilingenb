cbd02d90d149f73c885394a4e42d3591a12a23ee
Cédric <work@cedricverlinden.com>
2023-12-04 23:34:14 +0100

    Refactor translate function to fetch translations based on user's language

--- src/lib/util/translations.php
+++ src/lib/util/translations.php
@@ -1,4 +1,7 @@
-<?php 
-function translate($language) {
-  return fetch('SELECT id, ' . $language . ' FROM translations');
+<?php
+function translate($user, $str) {
+  $language = $user['language'];
+  $data = fetch('SELECT * FROM translations WHERE text_en = ?', ['type' => 's', 'value' => $str]);
+
+  return (empty($data) ? $str : $data[$language]);
 }
